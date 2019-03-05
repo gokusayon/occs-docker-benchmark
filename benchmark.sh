b7838c066b3fa82111d8ae345384d2bc89f4aa72
Vasu Sheoran <gokusayon@gmail.com>
2019-03-05 16:19:58 +0530

    Updated DockerFile

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo sh -c 'usermod -aG docker opc';
 sudo sh -c 'for n in $(seq 1 10); do docker run -it -e LINPACK_ARRAY_SIZE=600 h20180061/linpack; done';
 sudo sh -c 'for n in $(seq 1 10); do cd /usr/local/sbin/ && docker run -it polinux/bonnie bonnie++ -u 0:0 -s 1024 -r 512 ; done';
